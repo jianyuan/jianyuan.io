f6876b9c40f227765f11f86ea45bb654ea364b62
Jian Yuan Lee <jianyuan@gmail.com>
2024-01-15 22:44:18 +0000

    ref: posthog uses rev proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: "/ingest/:path*",
+      destination: "https://app.posthog.com/:path*",
+    },
+  ],
+};
 
 export default withContentlayer(nextConfig);
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,13 +4,10 @@ import { ThemeProvider } from "next-themes";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (
-  typeof window !== "undefined" &&
-  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
-  process.env.NEXT_PUBLIC_POSTHOG_HOST
-) {
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: `https://jianyuan.io/ingest`,
+    ui_host: "https://app.posthog.com",
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
 }
