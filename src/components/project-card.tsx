e7877cc7b3e61a70741bb8d9b71ebcb01de53bf9
Jian Yuan Lee <jianyuan@gmail.com>
2024-07-24 17:35:20 +0100

    ref: card styles

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,7 +4,7 @@ export default function ProjectCard({ project }: { project: Project }) {
   return (
     <a
       href={project.url}
-      className="space-y-1 group transition text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300"
+      className="space-y-1 group transition text-zinc-600 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700 bg-zinc-100 hover:bg-zinc-200 flex flex-col px-4 py-3 rounded-md"
       target="_blank"
       rel="noreferrer"
     >
